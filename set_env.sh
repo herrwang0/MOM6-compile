4c879cfea96a983c341acd793130f635efccc946
He Wang <he.wang@noaa.gov>
2024-10-15 16:04:05 -0400

    Fix boolean

--- set_env.sh
+++ set_env.sh
@@ -14,7 +14,7 @@ if [[ ${kernel} =~ Darwin ]] ; then # MacOS
     blddir=$HOME/builds
     if [[ ${compiler} =~ gcc ]] ; then
         source osx-gcc.env
-        use_default_templates=True
+        use_default_templates=true
         mkmf_temp=osx-gcc10.mk
     fi
 elif [[ ${kernel} =~ Linux ]] ; then # Linux
@@ -24,7 +24,7 @@ elif [[ ${kernel} =~ Linux ]] ; then # Linux
         blddir=$SCRATCH/$USER/gfdl_o/builds
         if [[ ${compiler} =~ intel ]] ; then
             source ./ncrc5-intel.env
-            use_default_templates=True
+            use_default_templates=true
             mkmf_temp=ncrc5-intel-classic.mk
         fi
     fi
@@ -33,7 +33,7 @@ elif [[ ${kernel} =~ Linux ]] ; then # Linux
         blddir=$SCRATCH/$USER/gfdl/builds
         if [[ ${compiler} =~ intel ]] ; then
             source ./ncrc6.intel23.env
-            use_default_templates=False
+            use_default_templates=false
             mkmf_temp=ncrc6.intel23.mk
         fi
     fi
@@ -41,7 +41,7 @@ elif [[ ${kernel} =~ Linux ]] ; then # Linux
     if [[ ${hostname} =~ ^gl ]] ; then
         if [[ ${compiler} =~ intel ]] ; then
             source ./greatlakes-intel.env
-            use_default_templates=False
+            use_default_templates=false
             mkmf_temp=greatlakes-intel.mk
         fi
     fi
@@ -51,13 +51,13 @@ else
 fi
 
 # Use codes shipped with MOM6-examples
-if [ ${use_eg} == True ] ; then
+if [ ${use_eg} == true ] ; then
     srcdir=${srcdir}/MOM6-examples/src
 fi
 
 # mkmf
 dir_mkmf=${srcdir}/mkmf
-if [ ${use_default_templates} == True ] ; then
+if [ ${use_default_templates} == true ] ; then
     mkmf_temp=${dir_mkmf}/templates/${mkmf_temp}
 else
     mkmf_temp=${PWD}/${mkmf_temp}
